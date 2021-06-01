2439dc598847cda4732ed0be381ac21d843c91e5
bhanu123456 <bhanupratap636@gmail.com>
2021-06-01 13:57:28 +0530

    registration validation setup

--- routes/User.js
+++ routes/User.js
@@ -7,6 +7,7 @@ const token_key = process.env.TOKEN_KEY;
 
 // import user model
 const User = require('./../models/User');
+const { findOne } = require('./../models/User');
 
 
 // middleware setup
@@ -20,6 +21,74 @@ router.get(
         status: true,
         message: "Default User API Route."
       });
+    }
+  );
+
+  router.post(
+    "/register",
+    [ 
+      check("username").not().isEmpty().withMessage("Please enter your username").trim().escape(),
+      check("password").not().isEmpty().withMessage("Please enter your password").trim().escape(),
+      check("password1").not().isEmpty().withMessage("Please enter your re-type password").trim().escape(),
+      check("email").isEmail().normalizeEmail().withMessage("Please enter your valid email"),
+    ],
+    (req, res) => {
+      console.log(req.body)
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+          let error={};
+          for (let index = 0; index < errors.array().length; index++) {
+            error ={
+              ...error,
+              [errors.array()[index].param]: errors.array()[index].msg
+            }
+            
+          }
+          return res.status(400).json({
+            status: false,
+            message: "Form Vlidation Error",
+            error: error
+          });
+      }
+
+      if(req.body.password !=req.body.password1){
+        return res.status(400).json({
+          status: false,
+          message: "Form Vlidation Error",
+          error: {
+            password1:"Re-type password is same as password"
+          }
+        });
+      }
+
+      User.findOne({email:req.body.email}).then(user =>{
+          if(user){
+            return res.status(400).json({
+              status: false,
+              message: "User already exists",
+            });
+          }else{
+            let salt = bcrypt.genSaltSync(10);
+            let hashedPassword = bcrypt.hashSync(req.body.password, salt);
+  
+            return res.status(200).json({
+              status: true,
+              message: "Default User API Route.",
+              password: hashedPassword
+            });
+          }
+
+         
+      }).catch(error =>{
+          return res.status(502).json({
+            status: true,
+            message: "Database error.",
+            error:{
+              db_error: "some error in database"
+            }
+          });
+      })
+
     }
   );
 
