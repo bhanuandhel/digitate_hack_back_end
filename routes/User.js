75f8c15059e22e94876a168292f975e33e40da5d
bhanu123456 <bhanupratap636@gmail.com>
2021-06-01 15:47:31 +0530

    register user data save

--- routes/User.js
+++ routes/User.js
@@ -15,82 +15,101 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.get(
-    "/",
-    (req, res) => {
-      return res.status(200).json({
-        status: true,
-        message: "Default User API Route."
+  "/",
+  (req, res) => {
+    return res.status(200).json({
+      status: true,
+      message: "Default User API Route."
+    });
+  }
+);
+
+router.post(
+  "/register",
+  [
+    check("username").not().isEmpty().withMessage("Please enter your username").trim().escape(),
+    check("password").not().isEmpty().withMessage("Please enter your password").trim().escape(),
+    check("password1").not().isEmpty().withMessage("Please enter your re-type password").trim().escape(),
+    check("email").isEmail().normalizeEmail().withMessage("Please enter your valid email"),
+  ],
+  (req, res) => {
+    
+    // check validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      let error = {};
+      for (let index = 0; index < errors.array().length; index++) {
+        error = {
+          ...error,
+          [errors.array()[index].param]: errors.array()[index].msg
+        }
+
+      }
+      return res.status(400).json({
+        status: false,
+        message: "Form Vlidation Error",
+        error: error
       });
     }
-  );
+// validation compare
+    if (req.body.password != req.body.password1) {
+      return res.status(400).json({
+        status: false,
+        message: "Form Vlidation Error",
+        error: {
+          password1: "Re-type password is same as password"
+        }
+      });
+    }
 
-  router.post(
-    "/register",
-    [ 
-      check("username").not().isEmpty().withMessage("Please enter your username").trim().escape(),
-      check("password").not().isEmpty().withMessage("Please enter your password").trim().escape(),
-      check("password1").not().isEmpty().withMessage("Please enter your re-type password").trim().escape(),
-      check("email").isEmail().normalizeEmail().withMessage("Please enter your valid email"),
-    ],
-    (req, res) => {
-      console.log(req.body)
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-          let error={};
-          for (let index = 0; index < errors.array().length; index++) {
-            error ={
-              ...error,
-              [errors.array()[index].param]: errors.array()[index].msg
-            }
-            
-          }
-          return res.status(400).json({
-            status: false,
-            message: "Form Vlidation Error",
-            error: error
-          });
-      }
-
-      if(req.body.password !=req.body.password1){
+    // check user in database
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
         return res.status(400).json({
           status: false,
-          message: "Form Vlidation Error",
-          error: {
-            password1:"Re-type password is same as password"
-          }
+          message: "User already exists",
         });
-      }
+      } else {
+        let salt = bcrypt.genSaltSync(10);
+        let hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
-      User.findOne({email:req.body.email}).then(user =>{
-          if(user){
-            return res.status(400).json({
-              status: false,
-              message: "User already exists",
-            });
-          }else{
-            let salt = bcrypt.genSaltSync(10);
-            let hashedPassword = bcrypt.hashSync(req.body.password, salt);
-  
-            return res.status(200).json({
-              status: true,
-              message: "Default User API Route.",
-              password: hashedPassword
-            });
-          }
+        // save new user
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword
+        })
 
-         
-      }).catch(error =>{
+        newUser.save().then(user => {
+          return res.status(200).json({
+            status: true,
+            message: "User register success.",
+            user: user
+          });
+        }).catch(error => {
           return res.status(502).json({
             status: true,
             message: "Database error.",
-            error:{
+            error: {
               db_error: "some error in database"
             }
           });
-      })
+        })
+      }
 
-    }
-  );
 
-  
+    }).catch(error => {
+      return res.status(502).json({
+        status: true,
+        message: "Database error.",
+        error: {
+          db_error: "some error in database"
+        }
+      });
+    })
+
+  }
+);
+
+
 module.exports = router;
